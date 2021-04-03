72e356a6b7c28e9cb3c962badd78ad3e822b35bb
Albert VDW <albert@avdw.com.au>
2021-04-03 22:11:00 +1000

    hamburger nav working

--- themes/v2/assets/js/codingo.js
+++ themes/v2/assets/js/codingo.js
@@ -1,7 +1,33 @@
+
+
+// Polygon Rendering
 import { default as Poly } from './polygons';
-
-var container = document.getElementById('fss-container');
-
+let container = document.getElementById('fss-container');
 if(container){
     Poly.initialise(container);
 }
+
+
+// Hamburger menu
+let hamburger = document.querySelector('.navigation-menu .hamburger');
+let navigationMenu = document.querySelector('.navigation-menu');
+let navigationList = document.querySelector('.navigation-menu .menu-list');
+hamburger.addEventListener("click", (event)=>{
+    event.preventDefault();
+    navigationList.classList.toggle("hidden");
+})
+// If clicked outside, then close the menu
+document.addEventListener("click", (event)=>{
+    const flyoutElement = navigationMenu;
+    let targetElement = event.target; 
+    do {
+        if (targetElement == flyoutElement) {
+            return;
+        }
+        targetElement = targetElement.parentNode;
+    } while (targetElement);
+    if(!navigationList.classList.contains('hidden')){
+        navigationList.classList.add("hidden");
+    }
+})
+
