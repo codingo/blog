792c8b90851f289cc4685dedb7710c42a3522f96
Albert VDW <albert@avdw.com.au>
2021-04-28 01:19:09 +1000

    search box added + results filtering

--- themes/v2/assets/js/codingoSearch.js
+++ themes/v2/assets/js/codingoSearch.js
@@ -5,18 +5,42 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const searchClient = algoliasearch('', '');
 
     const search = instantsearch({
-    indexName: '',
-    searchClient,
+        indexName: 'dev_blog',
+        searchClient,
     });
 
     search.addWidgets([
-    instantsearch.widgets.searchBox({
-        container: '#searchbox',
-    }),
 
-    instantsearch.widgets.hits({
-        container: '#hits',
-    })
+        instantsearch.widgets.configure({
+            hitsPerPage: 3
+        }),
+
+        instantsearch.widgets.searchBox({
+            container: '#searchbox',
+            showLoadingIndicator: true,
+            searchAsYouType: true
+        }),
+        
+        instantsearch.widgets.hits({
+            container: '#hits',
+            templates: {
+                empty: 'No results for <q>{{ query }}</q>',
+                item(hit, bindEvent) {
+                    console.log(hit)
+                  return `
+                    <a href="${hit.url}" ${bindEvent('conversion', hit, 'Product Added')} >
+                      <article>
+                        <h3>${hit.title}
+                        ${instantsearch.highlight({ attribute: 'name', hit })}</h3>
+            
+                        
+                            
+                        </button>
+                      </article>
+                    `;
+                }
+            }
+        })
     ]);
 
     search.start();
